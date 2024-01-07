72510c5c74fa1f0aecd591a14e1a294a0550ad9d
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2024-01-07 11:24:24 -0300

    fix: singup

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -5,26 +5,30 @@ import userMiddlewares from "@/middlewares/users-middlewares";
 
 const userController = {
     create: async (req: Request, res: Response, next: NextFunction) => {
+        if (res.headersSent) { // Verifica se uma resposta já foi enviada
+            return;
+        }
         try {
             await userMiddlewares.create(req, res, next);
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(req.body.senha, salt);
-            const usuario = {
-                nome: req.body.nome,
-                matricula: req.body.matricula,
-                senha: hashedPassword,
-                telefone: req.body.telefone,
+            if (res.headersSent) {
+                return;
+            }
+            const salt = 10;
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            const userdb = {
+                name: req.body.name,
+                password: hashedPassword,
                 email: req.body.email,
-                user_image: req.body.user_image,
-                role: req.body.role,
             };
-            const response = await user.create(usuario);
-            res.status(201).json({ response, message: "Usuário Registrado com Sucesso" });
+            const response = await user.create(userdb);
+            return res.status(201).send({ response, message: "Usuário Registrado com Sucesso" });
         } catch (error) {
-            res.status(500).json({ error, message: "Login Já Cadastrado no Sistema" });
-            console.log("Erro controller usuario\n" + error);
+            console.error("Erro controller usuario\n" + error);
+            if (!res.headersSent) {
+                return res.status(500).send({ error });
+            }
         }
     },
 }
-    
+
 export default userController;

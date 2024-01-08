305704439f9b357d1f714f9ba6646b19091358d1
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2024-01-07 23:24:41 -0300

    feat:populateDB

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import router from 'express';
 import user from './user-routes';
+import { populateDB } from '@/controllers/populateDB';
 
 const routes = router();
 
 routes.use('/', user);
+routes.post('/populate', populateDB);
 
 export default routes;
